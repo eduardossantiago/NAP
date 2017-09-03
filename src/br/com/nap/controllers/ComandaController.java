734315d11412d8f40378c4aa8b8a57154b7975b0
Eduardo <eduardossantiago@gmail.com>
2017-09-03 20:56:36 -0300

    Modificação ComandaController

--- src/br/com/nap/controllers/ComandaController.java
+++ src/br/com/nap/controllers/ComandaController.java
@@ -4,9 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.nap.model.Comanda;
+import br.com.nap.model.ComandaDAO;
 
 @Controller
 public class ComandaController {
+	
+	ComandaDAO dao;
+	
+	public ComandaController(ComandaDAO dao) {
+		
+		this.dao = dao;
+		
+	}
 
 	@RequestMapping("comanda")//pagina para digitar o numero da comanda
 	public String comanda() {
@@ -17,6 +26,14 @@ public class ComandaController {
 	@RequestMapping("abrirComanda")
 	public String abrirComanda(Comanda comanda) {
 		
-		return "teste";
+		if(dao.verStatus(comanda) == 1) {//se comanda estiver em uso
+			
+			return "comanda-em-uso";
+			
+		}else {
+			
+		}
+		
+			return "lista-atendimentos";
 	}
 }
